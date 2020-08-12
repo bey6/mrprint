1dc4603463a0d2c52238e7f16ac873dd033a9dd1
bey <bey.liu@outlook.com>
2020-08-13 00:00:30 +0800

    refactor: code merge.

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {View, Text, Button, TextInput, Pressable, Alert} from 'react-native'
+import {Image, View, Text, Button, TextInput, StyleSheet} from 'react-native'
 import Styles from './src/styles/style'
 
 const App = () => {
     const [value, onChangeText] = React.useState('')
     return (
-        <View>
+        <View style={localStyles.page}>
             <Text style={Styles.Text}>姓名</Text>
             <TextInput
                 style={Styles.TextInput}
@@ -17,10 +17,18 @@ const App = () => {
             <Text style={Styles.Text}>身份证人像面</Text>
             <Text style={Styles.Text}>身份证国徽面</Text>
             <Text style={Styles.Text}>手持身份证人像面</Text>
-            <Pressable onPress={onPress}>
-                <Text>下一步</Text>
-            </Pressable>
-            <Button title="下一步" />
+            <Button
+                style={localStyles.btnNext}
+                title="下一步"
+                onPress={onPress}
+            />
+            <Image
+                style={localStyles.avatar}
+                source={{
+                    uri:
+                        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597257540139&di=70876025eed64c4ff327c46f55f460a7&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201701%2F24%2F20170124185729_XKjME.thumb.700_0.jpeg',
+                }}
+            />
         </View>
     )
 }
@@ -28,5 +36,24 @@ const App = () => {
 function onPress() {
     alert('abc')
 }
+
+const localStyles = StyleSheet.create({
+    page: {
+        height: '100%',
+        backgroundColor: '#fafafa',
+        position: 'relative',
+    },
+    btnNext: {
+        position: 'absolute',
+        width: 100,
+        height: 100,
+        bottom: 10,
+    },
+    avatar: {
+        borderRadius: 50,
+        width: 100,
+        height: 100,
+    },
+})
 
 export default App
